6170fd06d877968f9114bb37faaf8b37a7bba177
Роман Решетов <romanreshetov@MacBook-Pro-Roman.local>
2024-01-28 18:45:11 +0300

    minor refactoring

--- src/shared/ui/AppLink/ui/AppLink.stories.ts
+++ src/shared/ui/AppLink/ui/AppLink.stories.ts
@@ -1,6 +1,6 @@
 import type { Meta, StoryObj } from '@storybook/react';
-import 'app/styles/index.scss';
 import { Theme } from 'app/providers/ThemeProvider';
+import 'app/styles/index.scss';
 import { ThemeDecorator } from 'shared/config/storybook/ThemeDecorator/ThemeDecorator';
 import { AppLink, AppLinkTheme } from './AppLink';
 
